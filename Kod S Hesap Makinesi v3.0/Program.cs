bd0654d633b1695f27b07fd955c0d649aab0d718
Murat Bilginer <muratbilginer09@gmail.com>
2020-03-23 02:26:48 +0300

    Initial commit

---
+++ Kod S Hesap Makinesi v3.0/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Kod_S_Hesap_Makinesi_v3._0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b, s;
+            string opt = "";
+
+            Console.WriteLine("Birinci sayıyı giriniz=");
+            a = double.Parse(Console.ReadLine());
+            Console.WriteLine("İkinci sayıyı giriniz=");
+            b = double.Parse(Console.ReadLine());
+            Console.WriteLine("Operatörünüzü giriniz:");
+            opt = (Console.ReadLine());
+
+            switch (opt)
+            {
+                case "+":
+                    {
+                        s = a + b;
+                        Console.WriteLine("Sonuç=" + s);
+
+                        break;
+                    }
+                case "-":
+                    {
+                        if (a > b)
+                            s = a - b;
+                        else if (b > a)
+                            s = b - a;
+                        else
+                            s = a - b;
+                        Console.WriteLine("Sonuç=" + s);
+                        break;
+                    }
+                case "*":
+                    {
+                        s = a * b;
+                        Console.WriteLine("Sonuç=" + s);
+                        break;
+                    }
+
+                case "/":
+                    {
+                        if (a / b == 0)
+                        {
+                            s = a / b;
+
+                            Console.WriteLine("Sonuç=" + s);
+                        }
+
+                        else if (a / b != 0)
+                            Console.WriteLine("İfadeler tam bölünmüyor!!!");
+
+                        break;
+                    }
+
+                default:
+
+                    Console.WriteLine("Yanlış bir operatör girdiniz tekrar deneyiniz");
+                    break;
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# CSharp_Console_15_Hesap_Makinesi_v.3.0
+ 
